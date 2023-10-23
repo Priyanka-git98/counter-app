4bdfa0dde9447710657ef2bfa5a61a04007eef17
Priyanka-git98 <148323876+Priyanka-git98@users.noreply.github.com>
2023-10-23 15:19:02 +0530

    context Api, useContext code commited

--- src/App.js
+++ src/App.js
@@ -2,25 +2,25 @@ import React, { createContext } from 'react';
 // import Counter from './components/Counter'
 // import MyComponent from './components/DisableButton';
 // import Useref from './components/Useref'
-// import ChildA from './components/ChildA'
+import ChildA from './components/ChildA'
 // import Usememo from './components/Usememo'
 // import Usecallback from './components/Usecallback'
 
 
-// const data = createContext();
-// const data1 = createContext();
+const data = createContext();
+const data1 = createContext();
 function App() {
-  // const name = 'pancy'
-  // const gender = 'female'
+  const name = 'pancy'
+  const gender = 'female'
   return (
     <div className="App">
 
-{/* <data.Provider value={name}>
+<data.Provider value={name}>
 <data.Provider value={gender}>
-      <ChildA/> */}
+      <ChildA/>
   {/* <ChildA name={name}/> */}
-      {/* </data.Provider>
-      </data.Provider> */}
+      </data.Provider>
+      </data.Provider>
 
 
       {/* <Counter/> */}
@@ -34,5 +34,5 @@ function App() {
 }
 
 export default App;
-// export {data, data1};
+export {data, data1};
 
